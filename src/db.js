ae7b8155d7fd2f4071aa4444177c104bc8b036fe
tanuj <sum12ting@gmail.com>
2026-08-12 22:55:52 +0530

    testing pipeline

--- src/db.js
+++ src/db.js
@@ -2,6 +2,9 @@ import pg from "pg";
 import "dotenv/config";
 const { Pool } = pg;
 
+
+
+
 const pool = new Pool({
     host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || 5432,
